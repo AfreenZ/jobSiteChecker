9c813bb0d860433254d86eac7cb526f7ca726246
AfreenZ <79364351+AfreenZ@users.noreply.github.com>
2021-02-21 02:27:38 -0500

    Update backgroundscript.js

--- backgroundscript.js
+++ backgroundscript.js
@@ -1,10 +1,23 @@
 // insert list of trigger words 
-chrome.webviewTag.find(searchText: "Hire", matchCase, Notification )
+var trigger = ["hired immediately", "position guaranteed", "fee", "payment", "cash", "check", "wire money", "credit card", "no experience necessary", "social security number", "ssn"]; 
+var results = function(results) {
 
+// insert list of trigger words
 
+chrome.webviewTag.find(trigger, matchCase, function(results) { 
+  var numberoftriggers = results.trigger;
+  function triggerPresence() {
+    // Get a value saved in a form.
+    var thePresence = numberoftriggers.value;
 
-chrome.storage.sync.get('timestamps', function(result){
-    var temp = result.timestamps;
-    temp.push(new Date().toLocaleString());
-    chrome.storage.sync.set({'timestamps':temp}, function(){
-        console.log("timestamps set");
+    chrome.tabs.query({active:true}, function(tabs){
+      chrome.extension.onConnect.addListener(function(port){
+        console.log(numberoftriggers);
+        port.postMessage(numberoftriggers);
+      });
+    });
+}
+
+})
+
+}
